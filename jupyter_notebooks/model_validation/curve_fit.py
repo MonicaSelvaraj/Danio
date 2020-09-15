8446175d516391f0cb3c3f8df59775aee69364c0
MonicaSelvaraj <mselvaraj@wisc.edu>
2020-09-15 18:34:45 -0400

    merged curve fitting scripts

--- jupyter_notebooks/model_validation/curve_fit.py
+++ jupyter_notebooks/model_validation/curve_fit.py
@@ -1,6 +1,10 @@
 '''
-Usage: Fit curves in 2D first
-Combining the 2D fits to get a 3D fit
+This script performs curve fitting on the 2D projections of aggregates and combines the 2D fits to produce a 3D fit. 
+
+There are two modes to this script. If the first argument is 1, the script is used to estimate outlier thresholds for populations. 
+If the first argument is 2, the script is used to estimate the 95% CI. 
+
+args: 1 - mode, 2 - pop
 '''
 
 import sys
@@ -10,14 +14,19 @@ import common_funcs
 from scipy.optimize import curve_fit 
 
 '''
-Defining input functions to scipy.optimize.curvefit
+usage: defines cos input function to scipy.optimize.curvefit.
 '''
 def helixFitCos(pc1, r, pitch, phase):
     return r*np.cos(pc1*((2*np.pi)/pitch) + phase)
+
+'''
+usage: defines sin input function to scipy.optimize.curvefit.
+'''
 def helixFitSin(pc1, r, pitch, phase):
     return r*np.sin(pc1*((2*np.pi)/pitch) + phase)
+
 '''
-Given x, predict the best y 
+usage: predicts the best y, given x.
 '''
 def fitFunction(x, y, function):
     try:
@@ -28,8 +37,9 @@ def fitFunction(x, y, function):
         radius = popt[0]; pitch = popt[1]; phase = popt[2]
         StandardErr = np.sqrt(np.diag(pcov))
         return(radius, pitch, phase, StandardErr[0], StandardErr[1], StandardErr[2])
+    
 '''
-Comparing sin and cos fits
+usage: compares sin and cos fits and picks one
 '''
 def BestFit(x,y):
         CosFit = fitFunction(x, y, helixFitCos)
@@ -43,17 +53,36 @@ def BestFit(x,y):
                 yOpt = [helixFitSin(c1, SinFit[0], SinFit[1], SinFit[2]) for c1 in x]
                 return (SinFit[0],SinFit[1],SinFit[2],SinFit[3],SinFit[4],SinFit[5],yOpt)
         
-#Get input     
+#Read input     
 c1,c2,c3 = common_funcs.getPoints("projections.csv")
-c1 = np.array(c1, dtype = float); c2 = np.array(c2, dtype = float); c3 = np.array(c3, dtype = float)
-#Fit
-r2,pi2,ph2,rse2,pise2,phse2,fitc2 = BestFit(c1, c2) # Predicts C2 given C1
-r3,pi3,ph3,rse3,pise3,phse3,fitc3 = BestFit(c1, c3)  # Predicts C3 given C1
-#Store params
+#Predict c2 given c1
+r2,pi2,ph2,rse2,pise2,phse2,fitc2 = BestFit(c1, c2) 
+#Predict c3 given c1
+r3,pi3,ph3,rse3,pise3,phse3,fitc3 = BestFit(c1, c3)  # Predicts C3 
+#Compile params into one array
 fitParams = [r2,pi2,ph2,rse2,pise2,phse2,r3,pi3,ph3,rse3,pise3,phse3]
-
 #Write fit coordinates to file
 np.savetxt("fit.csv", np.column_stack((c1, np.array(fitc2, dtype=float), np.array(fitc3, dtype=float))), delimiter=",", fmt='%s')
-#Write params to file
-common_funcs.appendToCsv("params.csv", fitParams)
+
+#Mode for outlier threshold detection
+if(sys.argv[1] == 1): 
+    #Append params to params.csv
+    common_funcs.appendToCsv("params.csv", fitParams)
+
+#Mode to estimate 95% CI
+if(sys.argv[1] == 2):
+    #Append params only after checking that it's not an outlier 
+    pop = sys.argv[2] 
+    out_thresh_dict = common_funcs.outlierThresholdsDict()
+    threshold = float(out_thresh_dict[pop])
+    rse = float((rse2 + rse3)/2)
+    if(rse <= threshold):
+        common_funcs.appendToCsv("params.csv", fitParams)
+    else:
+        #Write outlier coordinates to outliers.csv
+        with open('outliers.csv','ab') as f:
+            np.savetxt(f, np.column_stack((c1, c2, c3)), delimiter=",", fmt='%s')
+        sys.exit(1)
+
+#Indicate success of curve fitting and not an outlier
 sys.exit(0)
